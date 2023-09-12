cbeabb3444770c4ef91a450712d076f8829e2742
Amin7775 <amin.dpi24@gmail.com>
2023-09-12 17:59:36 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h3 className='text-6xl bg-red-400'>Knowledge cafe</h3>
+      </div>
+       
+    </>
+  )
+}
+
+export default App
